a32001fdcb8304ae2e46afbe9d9c287603262977
skyblue1232 <97932282+skyblue1232@users.noreply.github.com>
2024-11-03 05:14:56 +0900

    수정

---
+++ movie/pages/Recommend/RecommendedMovies.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecommendedMovies = () => {
+  return <h1 style={{ fontSize: '50px' }}>..........Hello Hello Recommended</h1>;
+};
+
+export default RecommendedMovies;
